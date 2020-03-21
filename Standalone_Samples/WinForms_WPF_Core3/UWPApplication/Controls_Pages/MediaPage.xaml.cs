3c1418be7d38a23a683c824f0639665dc812e08f
Miguel Ramos <miguelrb@microsoft.com>
2020-03-20 17:17:57 -0700

    Adding Win2D Sample

--- Standalone_Samples/WinForms_WPF_Core3/UWPApplication/Controls_Pages/MediaPage.xaml.cs
+++ Standalone_Samples/WinForms_WPF_Core3/UWPApplication/Controls_Pages/MediaPage.xaml.cs
@@ -16,12 +16,24 @@ namespace Samples.ManagedUWP
             inkPresenter.InputDeviceTypes =
                 CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Touch;
             this.Loaded += MediaPage_Loaded;
+            this.Unloaded += MediaPage_Unloaded;
+        }
+
+        private void MediaPage_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.canvas.RemoveFromVisualTree();
+            this.canvas = null;
         }
 
         private void MediaPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             var testPanel = new Native_SwapChainPanel_Comp.CustomRenderer();
             swapChainPanelGrid.Children.Add(testPanel);
+        }
+
+        private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender,Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.DrawText("Hello, World!", 100, 100, Windows.UI.Colors.Black);
         }
     }
 }
